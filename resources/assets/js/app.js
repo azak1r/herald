c72dea33a9c463e93873287bbd33315df71396e0
Andre Peiffer <necrotex@gmail.com>
2017-08-16 12:04:57 +0200

    fixed timezones for editing events

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,12 +16,20 @@ flatpickr(".flatpickr", {
     minDate: 'today',
     altInput: true,
     altFormat: 'Y-m-d H:i',
+
     onChange: function(selectedDates, dateStr, fp) {
         if (!selectedDates.length)
             return;
         const ISODate = selectedDates[0].toISOString(); // iso date str
         $('#due').val(ISODate);
+    },
+    onReady: function (selectedDates, dateStr, instance) {
+        var moment = require('moment-timezone');
+
+        instance.setDate(moment.tz(moment.utc(dateStr), moment.tz.guess()).format());
+        $('#due').val(moment.utc(dateStr).format());
     }
+
 
 });
 
